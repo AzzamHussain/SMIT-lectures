2838fe6cf6603ab95438a2be2768f13687a5d004
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-11 00:48:59 +0500

    Maps

---
+++ SMIT8thclassMaps.dart
@@ -0,0 +1,9 @@
+void main(){
+  Map studentInfo={"name":"Azzam","RollNo":13,"Sec":"A","Hobbies":["Game","Reading"]}; //Map ,ay hmaray pas 2 cheezay hoti hay key and value
+  print(studentInfo["Sec"]);                   //Map k Andar list bh use karsaktay hay*/
+ // studentInfo["name":="Bilal"];             //putIfAbsent method //slide
+
+}
+
+
+//Map<String,dynamic> studentInfo={};
