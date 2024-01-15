e5e664de4767846653cd3b8856eac7d4f4cb6caf
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-16 01:07:13 +0500

    maps

---
+++ SMIT9thSession.dart
@@ -0,0 +1,13 @@
+void main(){
+    //Map student={'name':'Azzam','age':23};
+  //print('Map: ');
+ // List<Map<String,dynamic>> students=[{"name":"Ali","result":"pass"},
+  //{"name":"AHmed","result":"fail"},
+  //];                                                
+  //print(students[1]["result"]);
+  dynamic abc=[
+    [3,5,6],
+    {'hello':{2:['abc',2,3],1:{"1":0,1:"a"},}['1']}
+  ];
+  print(abc[0]);
+}
