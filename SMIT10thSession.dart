0b05e2259b78a63dd27c9b9871dd82ea9fee4659
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-18 00:10:04 +0500

    For Loop

---
+++ SMIT10thSession.dart
@@ -0,0 +1,22 @@
+ /*************LOOPS***************/
+
+  //for (var i = 0; i < 100; i++) { //Step:1 variable initialization
+    //print(i);                     //step:2 condition run
+  //}                               //step:3 run for body
+//}                                 //step:4 increment
+//void main(){
+  //for(var i=0;i>-5;i=i-1){
+    //print(i);
+  //}
+//}   
+void main(){
+  List students=[
+  {"Name":"Ahmed","age":20},
+  {"Name":"Azzam","age":10},
+  {"Name":"Ramis","age":28},
+
+  ];
+  for(var i=students[0]["Names"];i<students.length;i++){
+    print(students[i]);
+  }
+}                            
