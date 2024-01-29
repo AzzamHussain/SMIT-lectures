a62f022ab817248465a3d9b42e04913538c36293
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-29 20:08:26 +0500

    Practice

---
+++ SMIT13thSession.dart
@@ -0,0 +1,10 @@
+// void main(){
+//   List numbers=[3,5,7,-8,-6,12,89];
+// numbers.removeWhere((element) => element>0);
+// print(numbers);  
+// }
+void main() {
+  var vals2 = [-2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ vals2.removeWhere((e) => e > 0);
+   print(vals2);
+}
