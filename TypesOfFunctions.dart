9f9540110cf3e32e04bc780da8cc5254f864cbbc
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-02-01 00:05:01 +0500

    TypesOfFunctions

---
+++ TypesOfFunctions.dart
@@ -0,0 +1,11 @@
+void main(){
+  var abc=sumOfTwoNumbers();
+  print("value of abc:$abc");
+}
+sumOfTwoNumbers(){
+  int num1=6;
+  int num2=8;
+  int result=num1+num2;
+  print("value of result: $result");
+  return result;
+}
