b04f8ed2a479e418ae175e49f87f879b29cbc66f
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-02-21 08:41:22 +0500

    abstractClass

---
+++ abstractclass.dart
@@ -0,0 +1,47 @@
+//they cannot be initialized,they are declared using the keyword abstract
+
+
+abstract class Vehicle {
+  // Abstract method
+  void start();
+  // Abstract method
+  void stop();
+}
+
+class Car extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Car started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Car stopped');
+  }
+}
+
+class Bike extends Vehicle {
+  // Implementation of start()
+  @override
+  void start() {
+    print('Bike started');
+  }
+
+  // Implementation of stop()
+  @override
+  void stop() {
+    print('Bike stopped');
+  }
+}
+
+void main() {
+  Car car = Car();
+  car.start();
+  car.stop();
+
+  Bike bike = Bike();
+  bike.start();
+  bike.stop();
+}
