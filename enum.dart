b7fe4cb510d560bdb8145d56100b8dd2956547f7
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-02-22 23:51:47 +0500

    enum

---
+++ enum.dart
@@ -0,0 +1,38 @@
+//enum is a special type that is used to store constant values
+
+enum days {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
+}
+
+void main() {
+  var today = days.Friday;
+  switch (today) {
+    case days.Sunday:
+      print("Today is Sunday.");
+      break;
+    case days.Monday:
+      print("Today is Monday.");
+      break;
+    case days.Tuesday:
+      print("Today is Tuesday.");
+      break;
+    case days.Wednesday:
+      print("Today is Wednesday.");
+      break;
+    case days.Thursday:
+      print("Today is Thursday.");
+      break;
+    case days.Friday:
+      print("Today is Friday.");
+      break;
+    case days.Saturday:
+      print("Today is Saturday.");
+      break;
+  }
+}
