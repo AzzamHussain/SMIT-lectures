e3d68e279d9ad6c93ade0cfec6702722646fca54
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-02-22 23:56:21 +0500

    enumPractice

---
+++ enum2.dart
@@ -0,0 +1,23 @@
+enum Gender { Male, Female, Other }
+
+class Person {
+  String? firstName;
+  String? lastName;
+  Gender? gender;
+
+  Person(this.firstName, this.lastName, this.gender);
+
+  void display() {
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Gender: $gender");
+  }
+}
+
+void main() {
+  Person p1 = Person("John", "Doe", Gender.Male);
+  p1.display();
+
+  Person p2 = Person("Menuka", "Sharma", Gender.Female);
+  p2.display();
+}
